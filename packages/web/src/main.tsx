7777fa4b565c0ce889e83bbb419124a654c4f6c0
Carlos Santos <casantosmu@gmail.com>
2023-11-12 16:13:30 +0100

    Add MaterialUI && Remove vite boilerplate

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App.tsx";
-import "./index.css";
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
