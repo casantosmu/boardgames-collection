ee8021518aca645cca1c613b8cc17f36a06a5299
Carlos Santos <casantosmu@gmail.com>
2023-11-12 15:45:50 +0100

    Add init web package with vite and eslint

--- packages/api/src/routes/boardgames.ts
+++ packages/api/src/routes/boardgames.ts
@@ -11,53 +11,56 @@ export const boardgamesRoutes: FastifyPluginAsyncTypebox = async (fastify) => {
       schema: {
         description: "Get a list of boardgames in the collection",
         tags: ["boardgames"],
-        deprecated: true,
         response: {
-          200: Type.Array(
-            Type.Object(
-              {
-                id: Type.Integer(),
-                rate: Type.Union([Type.Number(), Type.Null()]),
-                name: Type.String(),
-                yearPublished: Type.Integer(),
-                imagePath: Type.String(),
-                description: Type.String(),
-                shortDescription: Type.Union([Type.String(), Type.Null()]),
-                complexity: Type.Number(),
-                minAge: Type.Integer(),
-                minPlayers: Type.Integer(),
-                maxPlayers: Type.Union([Type.Integer(), Type.Null()]),
-                minDuration: Type.Integer(),
-                maxDuration: Type.Integer(),
-              },
-              {
-                description: "Success",
-              },
-            ),
+          200: Type.Object(
+            {
+              data: Type.Array(
+                Type.Object({
+                  id: Type.Integer(),
+                  rate: Type.Union([Type.Number(), Type.Null()]),
+                  name: Type.String(),
+                  yearPublished: Type.Integer(),
+                  imagePath: Type.String(),
+                  description: Type.String(),
+                  shortDescription: Type.Union([Type.String(), Type.Null()]),
+                  complexity: Type.Number(),
+                  minAge: Type.Integer(),
+                  minPlayers: Type.Integer(),
+                  maxPlayers: Type.Union([Type.Integer(), Type.Null()]),
+                  minDuration: Type.Integer(),
+                  maxDuration: Type.Integer(),
+                }),
+              ),
+            },
+            {
+              description: "Success",
+            },
           ),
         },
       },
     },
     async () => {
-      return fastify.kysely
-        .selectFrom("boardgames")
-        .select([
-          "boardgameId as id",
-          "rate",
-          "boardgameName as name",
-          "yearPublished",
-          "imagePath",
-          "description",
-          "shortDescription",
-          "complexity",
-          "minAge",
-          "minPlayers",
-          "maxPlayers",
-          "minDuration",
-          "maxDuration",
-        ])
-        .limit(10)
-        .execute();
+      return {
+        data: await fastify.kysely
+          .selectFrom("boardgames")
+          .select([
+            "boardgameId as id",
+            "rate",
+            "boardgameName as name",
+            "yearPublished",
+            "imagePath",
+            "description",
+            "shortDescription",
+            "complexity",
+            "minAge",
+            "minPlayers",
+            "maxPlayers",
+            "minDuration",
+            "maxDuration",
+          ])
+          .limit(10)
+          .execute(),
+      };
     },
   );
 };
---
+++ packages/eslint-config-custom/react.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: { browser: true },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  plugins: ["react-refresh"],
+  rules: {
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    react: {
+      version: "detect", // React version. "detect" automatically picks the version you have installed.
+    },
+  },
+};
