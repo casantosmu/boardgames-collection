22b4749e7bd9930d44971634bd6eea186e450473
Carlos Santos <casantosmu@gmail.com>
2023-12-18 15:02:06 +0100

    Modify object curly newline

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,6 @@
 import { buildApp } from "./app.js";
 
 const app = await buildApp();
+const PORT = process.env["SERVER_PORT"] ? +process.env["SERVER_PORT"] : 3000;
 
-await app.listen({
-  port: process.env["SERVER_PORT"] ? +process.env["SERVER_PORT"] : 3000,
-});
+await app.listen({ port: PORT });
